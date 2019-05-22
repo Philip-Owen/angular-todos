1f9a38207ea2aa9f66bec9687b9c7faea65eeda5
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-05-22 15:43:16 -0500

    added eventemitter to pass todo input to todo container

--- src/app/todo/todo-container/todo-container.component.ts
+++ src/app/todo/todo-container/todo-container.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from '../../model/todo';
+
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
-  styleUrls: ['./todo-container.component.css']
+  styleUrls: ['./todo-container.component.css'],
 })
 export class TodoContainerComponent implements OnInit {
+  public todos: Array<Todo>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.todos = [];
   }
 
+  ngOnInit() {}
+
+  addTodo(todo: Todo) {
+    this.todos = [...this.todos, todo];
+  }
 }
--- src/app/todo/todo-input/todo-input.component.ts
+++ src/app/todo/todo-input/todo-input.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { Todo } from '../../model/todo';
 
 @Component({
   selector: 'app-todo-input',
@@ -7,12 +9,15 @@ import { Component } from '@angular/core';
 })
 export class TodoInputComponent {
   public todo: string;
+  @Output() private addTodo: EventEmitter<Todo>;
 
   constructor() {
     this.todo = '';
+    this.addTodo = new EventEmitter<Todo>();
   }
 
-  addTodo() {
-    console.log(this.todo);
+  onTodoSubmit(event) {
+    this.addTodo.emit(new Todo(this.todo))
+    this.todo = '';
   }
 }
