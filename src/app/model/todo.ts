fb77dbe8ece0226685e9a54fc5f0e4831a64db93
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-05-22 15:20:31 -0500

    added ngFor to display todos and styles. created todo model.

---
+++ src/app/model/todo.ts
@@ -0,0 +1,4 @@
+export class Todo {
+  completed: Boolean = false;
+  constructor(public task: string) {}
+}
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Todo } from '../../model/todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
+  public todos: Array<Todo>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.todos = [new Todo('Clean room'), new Todo('Learn more Angular'), new Todo('Build cool apps')];
   }
 
+  markCompleted(index) {
+    this.todos[index].completed = !this.todos[index].completed;
+  }
+
+  deleteTodo(todoTask) {
+    this.todos = this.todos.filter( t => t.task !== todoTask)
+  }
 }
